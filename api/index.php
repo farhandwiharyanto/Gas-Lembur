23428effec0f6a70e38460ad616d67142bc09958
farhandwiharyanto <farhandwiharyanto30@gmail.com>
2026-07-02 01:20:16 +0700

    fix: make api/index.php standalone to prevent download

--- api/index.php
+++ api/index.php
@@ -1,7 +1,13 @@
 <?php
 
-/**
- * Vercel Serverless Entry Point
- * This forwards all requests to the standard Laravel entry point.
- */
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Illuminate\Http\Request::capture());
